70051887cd00a87d7dba77bf1ccac5297b6fc930
Student <Student@C105_25>
2026-04-21 09:47:31 +0200

    Moving PacMan

--- EN_Lab_08_KeyEvents_Timer_Graphics_PacMan/Form1.cs
+++ EN_Lab_08_KeyEvents_Timer_Graphics_PacMan/Form1.cs
@@ -14,16 +14,61 @@ namespace EN_Lab_08_KeyEvents_Timer_Graphics_PacMan
     {
         private enum FieldType { Empty, Wall, Dot };
         private FieldType [,]board;
+
         private Point pacMan;
+        private Keys pacManDirection = Keys.None;
 
         private const int FIELD_SIZE = 50;
         public Form1()
         {
             InitializeComponent();
 
+            DoubleBuffered = true;
+
             this.Paint += Form1_Paint;
+            this.KeyDown += Form1_KeyDown;
 
             OpenMap(Properties.Resources.Map_1);
+
+            Timer gameTimer = new Timer();
+            gameTimer.Interval = 200;
+            gameTimer.Tick += GameTimer_Tick;
+            gameTimer.Start();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            switch(pacManDirection)
+            {
+                case Keys.Down:
+                    if (board[pacMan.Y + 1, pacMan.X] != FieldType.Wall) pacMan.Y++;
+                    break;
+                case Keys.Up:
+                    if (board[pacMan.Y - 1, pacMan.X] != FieldType.Wall) pacMan.Y--; 
+                    break;
+                case Keys.Left:
+                    if (board[pacMan.Y , pacMan.X - 1] != FieldType.Wall) pacMan.X--; 
+                    break;
+                case Keys.Right:
+                    if (board[pacMan.Y, pacMan.X + 1] != FieldType.Wall) pacMan.X++;
+                    break;
+            }
+            if(board[pacMan.Y, pacMan.X] == FieldType.Dot)
+            {
+                board[pacMan.Y, pacMan.X] = FieldType.Empty;
+            }
+            Invalidate();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Down ||
+                e.KeyCode == Keys.Up ||
+                e.KeyCode == Keys.Left ||
+                e.KeyCode == Keys.Right)
+            {
+                pacManDirection = e.KeyCode;
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
